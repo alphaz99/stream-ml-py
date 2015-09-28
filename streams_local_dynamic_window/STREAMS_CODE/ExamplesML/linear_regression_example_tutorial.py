6c0db3f2726f135c5ad45254ae0ce260655cdb25
Rahul Bachal <rbachal@caltech.edu>
2015-09-28 10:21:01 -0700

    Updated example directory for new name.

---
+++ streams_local_dynamic_window/STREAMS_CODE/ExamplesML/linear_regression_example_tutorial.py
@@ -0,0 +1,60 @@
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+from Stream import Stream
+from Operators import stream_func
+from ML import Stream_Learn, LinearRegression
+from ML.LinearRegression import linear_regression
+import numpy as np
+# Parameters
+
+draw = True
+output = False
+num_features = 1
+min_window_size = 2
+max_window_size = 50
+num_points = 1000
+step_size = 1
+
+if __name__ == "__main__":
+
+    i = 0
+    w = np.random.rand(num_features + 1, 1) * 2 - 1
+    w *= 5
+
+    m = LinearRegression.LinearRegressionStream(draw=draw, output=output,
+                                          alpha=0.001)
+
+    x = Stream('x')
+
+    linear_regression.init_plot()
+    model = Stream_Learn(data_train=x, data_out=x, train_func=m.train,
+                         predict_func=m.predict,
+                         min_window_size=min_window_size,
+                         max_window_size=max_window_size, step_size=step_size,
+                         num_features=num_features)
+    y = model.run()
+
+    while i < num_points:
+        z = np.random.rand(num_features + 1, 1) * 2 - 1
+        # w += z / 10
+        w[1] += 0.01
+        # x_value = np.random.rand(1, num_features) * 2 - 1
+        x_value = np.array([i]).reshape(1, 1)
+        x_b = np.hstack((np.ones((1, 1)), x_value)).transpose()
+        y_value = w.transpose().dot(x_b)[0][0]
+        values = x_value.tolist()[0]
+        values.append(y_value)
+        x.extend([tuple(values)])
+
+        if i % 100 == 0 and i != 0:
+            model.reset()
+
+        print i
+        i += 1
+
+    print "Average error: ", m.avg_error
